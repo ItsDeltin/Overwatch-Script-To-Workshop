8a9f929513e6abafd5b7f0ec30c29d4d814001a0
Deltin <grittydeltin@gmail.com>
2019-05-17 23:14:24 -0400

    Split constants and enums

---
+++ OverwatchParser/OverwatchParser/Elements/Enums.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverwatchParser.Elements
+{
+    public enum RuleEvent
+    {
+        Ongoing_Global,
+        Ongoing_EachPlayer,
+
+        Player_Earned_Elimination,
+        Player_Dealt_Final_Blow,
+
+        Player_Dealt_Damage,
+        Player_Took_Damage,
+
+        Player_Died
+    }
+
+    public enum PlayerSelector
+    {
+        All,
+        Slot0,
+        Slot1,
+        Slot2,
+        Slot3,
+        Slot4,
+        Slot5,
+        Slot6,
+        Slot7,
+        Slot8,
+        Slot9,
+        Slot10,
+        Slot11,
+        // Why isn't it alphabetical? we will never know.
+        Reaper,
+        Tracer,
+        Mercy,
+        Hanzo,
+        Torbjorn,
+        Reinhardt,
+        Pharah,
+        Winston,
+        Widowmaker,
+        Bastion,
+        Symmetra,
+        Zenyatta,
+        Gengi,
+        Roadhog,
+        Mccree,
+        Junkrat,
+        Zarya,
+        Soldier76,
+        Lucio,
+        Dva,
+        Mei,
+        Sombra,
+        Doomfist,
+        Ana,
+        Orisa,
+        Brigitte,
+        Moira,
+        WreckingBall,
+        Ashe,
+        Baptiste
+    }
+
+    [AttributeUsage(AttributeTargets.Enum)]
+    public class EnumParameter : Attribute
+    {
+        public EnumParameter() { }
+    }
+
+    [EnumParameter]
+    public enum Variable
+    {
+        A,
+        B,
+        C,
+        D,
+        E,
+        F,
+        G,
+        H,
+        I,
+        J,
+        K,
+        L,
+        M,
+        N,
+        O,
+        P,
+        Q,
+        R,
+        S,
+        T,
+        U,
+        V,
+        W,
+        X,
+        Y,
+        Z
+    }
+
+    [EnumParameter]
+    public enum Operators
+    {
+        Equal,
+        NotEqual,
+        LessThan,
+        LessThanOrEqual,
+        GreaterThan,
+        GreaterThanOrEqual
+    }
+
+    [EnumParameter]
+    public enum Operation
+    {
+        Add,
+        Subtract,
+        Multiply,
+        Divide,
+        Modulo,
+        RaiseToPower,
+        Min,
+        Max,
+        AppendToArray,
+        RemoveFromArrayByValue,
+        RemoveFromArrayByIndex
+    }
+
+    [EnumParameter]
+    public enum Button
+    {
+        PrimaryFire,
+        SecondaryFire,
+        Ability1,
+        Ability2,
+        Ultimate,
+        Interact,
+        Jump,
+        Crouch
+    }
+
+    [EnumParameter]
+    public enum Relative
+    {
+        ToWorld,
+        ToPlayer
+    }
+
+    [EnumParameter]
+    public enum ContraryMotion
+    {
+        Cancel,
+        Incorporate
+    }
+
+    [EnumParameter]
+    public enum ChaseReevaluation
+    {
+        DestinationAndRate,
+        None
+    }
+
+    [EnumParameter]
+    public enum Status
+    {
+        Hacked,
+        Burning,
+        KnockedDown,
+        Asleep,
+        Frozen,
+        Unkillable,
+        Invincible,
+        PhasedOut,
+        Rooted,
+        Stunned
+    }
+
+    [EnumParameter]
+    public enum TeamSelector
+    {
+        All,
+        Team1,
+        Team2,
+    }
+
+    [EnumParameter]
+    public enum WaitBehavior
+    {
+        IgnoreCondition,
+        AbortWhenFalse,
+        RestartWhenTrue
+    }
+
+    [EnumParameter]
+    public enum Effect
+    {
+        Sphere,
+        LightShaft,
+        Orb,
+        Ring,
+        Cloud,
+        Sparkles,
+        GoodAura,
+        BadAura,
+        EnergySound,
+        PickupSound,
+        GoodAuraSound,
+        BadAuraSound,
+        SparklesSound,
+        SmokeSound,
+        DecalSound,
+        BeaconSound
+    }
+
+    [EnumParameter]
+    public enum Color
+    {
+        White,
+        Yellow,
+        Green,
+        Purple,
+        Red,
+        Blue,
+        Team1,
+        Team2
+    }
+
+    [EnumParameter]
+    public enum EffectRev
+    {
+        VisibleToPositionAndRadius,
+        PositionAndRadius,
+        VisibleTo,
+        None
+    }
+
+    [EnumParameter]
+    public enum Rounding
+    {
+        Up,
+        Down,
+        Nearest
+    }
+}
