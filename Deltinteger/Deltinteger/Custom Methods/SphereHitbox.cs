d2fac01c94ee9558eb12ea1b2fbb2c927ed196f4
TrueCP5 <truecp5@gmail.com>
2019-08-11 09:28:19 +0200

    Added SphereHitbox

--- Deltinteger/Deltinteger/Custom Methods/SphereHitbox.cs
+++ Deltinteger/Deltinteger/Custom Methods/SphereHitbox.cs
@@ -3,16 +3,19 @@ using Deltin.Deltinteger.WorkshopWiki;
 
 namespace Deltin.Deltinteger.Elements
 {
-    [CustomMethod("EyeCastHitPosition", CustomMethodType.Value)]
+    [CustomMethod("SphereHitbox", CustomMethodType.Value)]
     [Parameter("player", ValueType.Player, null)]
-    [Parameter("range", ValueType.Number, null)]
-    class Test : CustomMethodBase
+    [Parameter("spherePosition", ValueType.VectorAndPlayer, null)]
+    [Parameter("sphereRadius", ValueType.Number, null)]
+    class SphereHitbox : CustomMethodBase
     {
         protected override MethodResult Get()
         {
             Element player = (Element)Parameters[0];
-            Element range = (Element)Parameters[1];
+            Element position = (Element)Parameters[1];
+            Element radius = (Element)Parameters[2];
             Element eyePos = Element.Part<V_EyePosition>(player);
+            Element range = Element.Part<V_DistanceBetween>(eyePos, position);
             Element direction = Element.Part<V_FacingDirectionOf>(player);
             Element raycast = Element.Part<V_RayCastHitPosition>(eyePos,
                 Element.Part<V_Add>(
@@ -23,12 +26,14 @@ namespace Deltin.Deltinteger.Elements
                     new V_Null(),
                     new V_False()
                 );
-            return new MethodResult(null, raycast);
+            Element distance = Element.Part<V_DistanceBetween>(position, raycast);
+            Element compare = Element.Part<V_Compare>(distance, EnumData.GetEnumValue(Operators.LessThanOrEqual), radius);
+            return new MethodResult(null, compare);
         }
 
         public override WikiMethod Wiki()
         {
-            return new WikiMethod("EyeCastHitPosition", "Casts a ray in the direction the player is facing with a certain range.", null);
+            return new WikiMethod("SphereHitbox", "Whether the given player is looking directly at a sphere with collision.", null);
         }
     }
 }
