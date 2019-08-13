3816b2870116eea03df64080c2e80b89b07004c0
TrueCP5 <truecp5@gmail.com>
2019-08-13 11:16:20 +0200

    Added IsAIAccurate

--- Deltinteger/Deltinteger/Custom Methods/IsAI.cs
+++ Deltinteger/Deltinteger/Custom Methods/IsAI.cs
@@ -28,4 +28,38 @@ namespace Deltin.Deltinteger.Elements
             return new WikiMethod("IsAIUnintrusive", "Whether the player is an AI or not. Works in less situations but much less intrusive. Requires the player to be spawned in.", null);
         }
     }
+
+    [CustomMethod("IsAIAccurate", CustomMethodType.Value)]
+    [Parameter("player", ValueType.Player, typeof(V_EventPlayer))]
+    class IsAIAccurate : CustomMethodBase
+    {
+        protected override MethodResult Get()
+        {
+            IndexedVar player = TranslateContext.VarCollection.AssignVar(Scope, "IsAIAccurate: player", TranslateContext.IsGlobal, null);
+            IndexedVar originalHero = TranslateContext.VarCollection.AssignVar(Scope, "IsAIAccurate: originalHero", TranslateContext.IsGlobal, null);
+            IndexedVar isAI = TranslateContext.VarCollection.AssignVar(Scope, "IsAIAccurate: isAI", TranslateContext.IsGlobal, null);
+
+            Element[] actions = ArrayBuilder<Element>.Build
+            (
+                player.SetVariable((Element)Parameters[0]),
+                originalHero.SetVariable(Element.Part<V_HeroOf>(player.GetVariable())),
+                Element.Part<A_SkipIf>(Element.Part<V_Not>(Element.Part<V_Compare>(originalHero.GetVariable(), EnumData.GetEnumValue(Operators.Equal), new V_Null())), new V_Number(2)),
+                isAI.SetVariable(new V_False()),
+                Element.Part<A_Skip>(new V_Number(4)),
+                Element.Part<A_ForcePlayerHero>(player.GetVariable(), EnumData.GetEnumValue(Hero.Ashe)),
+                isAI.SetVariable(Element.Part<V_Compare>(Element.Part<V_HeroOf>(player.GetVariable()), EnumData.GetEnumValue(Operators.NotEqual), EnumData.GetEnumValue(Hero.Ashe))),
+                Element.Part<A_ForcePlayerHero>(player.GetVariable(), originalHero.GetVariable()),
+                Element.Part<A_StopForcingHero>(player.GetVariable())
+            );
+
+            Element result = isAI.GetVariable();
+            
+            return new MethodResult(actions, result);
+        }
+
+        public override WikiMethod Wiki()
+        {
+            return new WikiMethod("IsAIAccurate", "Whether the player is an AI or not. Works in more situations but it is more intrusive.", null);
+        }
+    }
 }
