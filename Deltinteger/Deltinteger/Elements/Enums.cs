342d60b457ef052b5fb317b79b4a3af30c488516
TrueCP5 <truecp5@gmail.com>
2019-08-13 18:50:20 +0200

    Fixed tiny spelling error

--- Deltinteger/Deltinteger/Elements/Enums.cs
+++ Deltinteger/Deltinteger/Elements/Enums.cs
@@ -539,7 +539,7 @@ namespace Deltin.Deltinteger.Elements
         ArrowRight,
         [EnumOverride(null, "Arrow: Up")]
         ArrowUp,
-        Aterisk,
+        Asterisk,
         Bolt,
         Checkmark,
         Circle,
