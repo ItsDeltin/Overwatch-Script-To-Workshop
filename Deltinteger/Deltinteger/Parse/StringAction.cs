cfbd9ae1afac8a63db09d6884055a5f980ab3ef4
ItsDeltin <34138844+ItsDeltin@users.noreply.github.com>
2019-12-30 13:03:14 -0500

    Updated localized strings.

--- Deltinteger/Deltinteger/Parse/StringAction.cs
+++ Deltinteger/Deltinteger/Parse/StringAction.cs
@@ -5,16 +5,24 @@ using System.Text;
 using System.Text.RegularExpressions;
 using Deltin.Deltinteger.LanguageServer;
 using Deltin.Deltinteger.Elements;
+using CompletionItem = OmniSharp.Extensions.LanguageServer.Protocol.Models.CompletionItem;
+using CompletionItemKind = OmniSharp.Extensions.LanguageServer.Protocol.Models.CompletionItemKind;
 
 namespace Deltin.Deltinteger.Parse
 {
     public class StringAction : IExpression
     {
+        private static readonly CompletionItem[] StringCompletion = Constants.Strings.Select(str => new CompletionItem() {
+            Label = str,
+            Kind = CompletionItemKind.Text
+        }).ToArray();
+
         public string Value { get; }
         public bool Localized { get; }
         public IExpression[] FormatParameters { get; }
-        private CustomStringParse[] Sectioned;
         private DocRange StringRange { get; }
+        private CustomStringParse[] Sectioned;
+        private LocalizedString LocalizedString;
 
         // Normal
         public StringAction(ScriptFile script, DeltinScriptParser.StringContext stringContext, bool parse = true)
@@ -23,6 +31,11 @@ namespace Deltin.Deltinteger.Parse
             Localized = stringContext.LOCALIZED() != null;
             StringRange = DocRange.GetRange(stringContext.STRINGLITERAL());
             if (parse) ParseString(script);
+
+            if (Localized)
+            {
+                script.AddCompletionRange(new CompletionRange(StringCompletion, StringRange, CompletionRangeKind.ClearRest));
+            }
         }
         // Formatted
         public StringAction(ParseInfo parseInfo, Scope scope, DeltinScriptParser.Formatted_stringContext stringContext) : this(parseInfo.Script, stringContext.@string(), false)
@@ -35,13 +48,24 @@ namespace Deltin.Deltinteger.Parse
 
         private void ParseString(ScriptFile script)
         {
-            if (!Localized)
+            try
             {
-                try
+                if (!Localized)
                 {
                     Sectioned = ParseCustomString(Value, FormatParameters);
                 }
-                catch (StringParseFailedException ex)
+                else
+                {
+                    LocalizedString = ParseLocalizedString(Value, FormatParameters);
+                }
+            }
+            catch (StringParseFailedException ex)
+            {
+                if (ex.StringIndex == -1)
+                {
+                     script.Diagnostics.Error(ex.Message, StringRange);
+                }
+                else
                 {
                     int errorStart = StringRange.start.character + 1 + ex.StringIndex;
                     script.Diagnostics.Error(ex.Message, new DocRange(
@@ -50,7 +74,6 @@ namespace Deltin.Deltinteger.Parse
                     ));
                 }
             }
-            else throw new NotImplementedException();
         }
 
         static CustomStringParse[] ParseCustomString(string value, IExpression[] parameters)
@@ -130,6 +153,102 @@ namespace Deltin.Deltinteger.Parse
             }
             return strings;
         }
+        
+        private static readonly string[] searchOrder = Constants.Strings
+            .OrderByDescending(str => str.Contains("{0}"))
+            .ThenByDescending(str => str.IndexOfAny("-></*-+=()!?".ToCharArray()) != -1)
+            .ThenByDescending(str => str.Length)
+            .ToArray();
+
+        static LocalizedString ParseLocalizedString(string value, IExpression[] parameters, int depth = 0)
+        {
+            value = value.ToLower();
+            
+            //if (depth == 0)
+                //foreach(string multiword in multiwordStrings)
+                    //value = value.Replace(multiword.Replace('_', ' '), multiword);
+
+            // Loop through every string to search for.
+            for (int i = 0; i < searchOrder.Length; i++)
+            {
+                string searchString = searchOrder[i];
+
+                // Converts string parameters ({0}, {1}, {2}) to regex expressions to get the values.
+                // {#} -> (.+)
+                string regex =
+                    Regex.Replace(Escape(searchString)
+                    , "({[0-9]})", @"(([a-z_.<>0-9-]+ ?)|(.+))");
+
+                // Add the regex expressions start-of-line and end-of-line to ensure that the entire string is parsed.
+                regex = "^" + regex + "$";
+
+                // Match
+                var match = Regex.Match(value, regex);
+                if (match.Success)
+                {
+                    // Create a string element with the found string.
+                    LocalizedString str = new LocalizedString(searchString);
+
+                    bool valid = true; // Confirms that the arguments were able to successfully parse.
+                    List<LocalizedStringOrExpression> formatParameters = new List<LocalizedStringOrExpression>(); // The parameters that were successfully parsed.
+
+                    // Iterate through the parameters.
+                    for (int g = 1; g < match.Groups.Count; g+=3)
+                    {
+                        string currentParameterValue = match.Groups[g].Captures[0].Value;
+
+                        // Test if the parameter is a format parameter, for example <0>, <1>, <2>, <3>...
+                        Match parameterString = Regex.Match(currentParameterValue, "^<([0-9]+)>$");
+                        if (parameters != null && parameterString.Success)
+                        {
+                            int index = int.Parse(parameterString.Groups[1].Value);
+
+                            // Throw syntax error if the number of parameters is less than the parameter index being set.
+                            if (index >= parameters.Length)
+                                throw new StringParseFailedException($"Can't set the <{index}> format, there are only {parameters.Length} parameters.");
+
+                            formatParameters.Add(new LocalizedStringOrExpression(parameters[index]));
+                        }
+                        else
+                        {
+                            // Parse the parameter. If it fails it will return null and the string being checked is probably false.
+                            var p = ParseLocalizedString(currentParameterValue, parameters, depth + 1);
+                            if (p == null)
+                            {
+                                valid = false;
+                                break;
+                            }
+                            formatParameters.Add(new LocalizedStringOrExpression(p));
+                        }
+                    }
+                    str.ParameterValues = formatParameters.ToArray();
+
+                    if (!valid)
+                        continue;
+                    
+                    return str;
+                }
+            }
+
+            if (depth > 0)
+                return null;
+            else
+                // If the depth is 0, throw a syntax error.
+                throw new StringParseFailedException("Failed to parse the string.");
+        }
+
+        static string Escape(string value)
+        {
+            return value
+                .Replace("?", @"\?")
+                .Replace("*", @"\*")
+                .Replace("(", @"\(")
+                .Replace(")", @"\)")
+                .Replace(".", @"\.")
+                .Replace("/", @"\/")
+                .Replace("+", @"\+")
+                ;
+        }
 
         public Scope ReturningScope() => null;
         public CodeType Type() => null;
@@ -144,7 +263,10 @@ namespace Deltin.Deltinteger.Parse
                 
                 return V_CustomString.Join(parsed);
             }
-            else throw new NotImplementedException();
+            else
+            {
+                return LocalizedString.Parse(actionSet);
+            }
         }
     }
 
@@ -186,6 +308,40 @@ namespace Deltin.Deltinteger.Parse
         {
             Formats = formats;
             EndIndex = formats.Last().Match.Index + formats.Last().Match.Length;
+        }
+    }
+
+    class LocalizedString
+    {
+        public string String { get; }
+        public LocalizedStringOrExpression[] ParameterValues { get; set; }
+
+        public LocalizedString(string str)
+        {
+            String = str;
+        }
+
+        public IWorkshopTree Parse(ActionSet actionSet) => new V_String(String, ParameterValues.Select(pv => (Element)pv.Parse(actionSet)).ToArray());
+    }
+
+    class LocalizedStringOrExpression
+    {
+        public LocalizedString LocalizedString { get; }
+        public IExpression Expression { get; }
+
+        public LocalizedStringOrExpression(LocalizedString str)
+        {
+            LocalizedString = str;
+        }
+        public LocalizedStringOrExpression(IExpression expression)
+        {
+            Expression = expression;
+        }
+
+        public IWorkshopTree Parse(ActionSet actionSet)
+        {
+            if (LocalizedString != null) return LocalizedString.Parse(actionSet);
+            else return Expression.Parse(actionSet);
         }
     }
 }
