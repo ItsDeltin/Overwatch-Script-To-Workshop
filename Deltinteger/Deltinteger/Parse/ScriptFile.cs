c22b67042369d46e4390ccd1b9e004e0247f9be7
ItsDeltin <34138844+ItsDeltin@users.noreply.github.com>
2020-02-10 22:37:46 -0500

    Moved parsing codelens data from ScriptFile.cs to CodeLens.cs

--- Deltinteger/Deltinteger/Parse/ScriptFile.cs
+++ Deltinteger/Deltinteger/Parse/ScriptFile.cs
@@ -1,18 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.IO;
-using Deltin.Deltinteger.Elements;
 using Deltin.Deltinteger.LanguageServer;
 using Antlr4.Runtime;
 using Antlr4.Runtime.Tree;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using LocationLink = OmniSharp.Extensions.LanguageServer.Protocol.Models.LocationLink;
 using CompletionItem = OmniSharp.Extensions.LanguageServer.Protocol.Models.CompletionItem;
 using CompletionItemKind = OmniSharp.Extensions.LanguageServer.Protocol.Models.CompletionItemKind;
-using LSLocation = OmniSharp.Extensions.LanguageServer.Protocol.Models.Location;
-using LSContainer = OmniSharp.Extensions.LanguageServer.Protocol.Models.Container<OmniSharp.Extensions.LanguageServer.Protocol.Models.Location>;
 
 namespace Deltin.Deltinteger.Parse
 {
@@ -145,97 +139,5 @@ namespace Deltin.Deltinteger.Parse
             Range = range;
             Content = content;
         }
-    }
-
-    [Flags]
-    public enum CodeLensSourceType
-    {
-        Function = 1,
-        Type = 2,
-        EnumValue = 4,
-        Variable = RuleVariable | ClassVariable | ScopedVariable | ParameterVariable,
-        RuleVariable = 8,
-        ClassVariable = 16,
-        ScopedVariable = 32,
-        ParameterVariable = 64,
-    }
-
-    public abstract class CodeLensRange
-    {
-        public CodeLensSourceType SourceType { get; }
-        public DocRange Range { get; }
-        public string Command { get; }
-
-        public CodeLensRange(CodeLensSourceType sourceType, DocRange range, string command)
-        {
-            SourceType = sourceType;
-            Range = range;
-            Command = command;
-        }
-
-        public abstract string GetTitle();
-
-        public virtual bool ShouldUse() => true;
-
-        public virtual JArray GetArguments() => new JArray();
-    }
-
-    class ReferenceCodeLensRange : CodeLensRange
-    {
-        /*
-        editor.action.showReferences - Show references at a position in a file
-            uri - The text document in which to show references
-            position - The position at which to show
-            locations - An array of locations.
-        */
-
-        public ICallable Callable { get; }
-        private readonly ParseInfo _parseInfo;
-
-        public ReferenceCodeLensRange(ICallable callable, ParseInfo parseInfo, CodeLensSourceType sourceType, DocRange range) : base(sourceType, range, "ostw.showReferences")
-        {
-            Callable = callable;
-            _parseInfo = parseInfo;
-        }
-
-        public override string GetTitle() => (_parseInfo.TranslateInfo.GetSymbolLinks(Callable).Count - 1).ToString() + " references";
-
-        public override JArray GetArguments() => new JArray {
-            // Uri
-            JToken.FromObject(_parseInfo.Script.Uri.ToString()),
-            // Range
-            JToken.FromObject(Range.start),
-            // Locations
-            JToken.FromObject(_parseInfo.TranslateInfo.GetSymbolLinks(Callable).GetSymbolLinks(false).Select(sl => sl.Location))
-        };
-    }
-
-    class ImplementsCodeLensRange : CodeLensRange
-    {
-        public IMethod Method { get; }
-        private readonly ScriptFile _script;
-
-        public ImplementsCodeLensRange(IMethod method, ScriptFile script, CodeLensSourceType sourceType, DocRange range) : base(sourceType, range, "ostw.showReferences")
-        {
-            Method = method;
-            _script = script;
-        }
-
-        public override bool ShouldUse()
-        {
-            // Only show the codelens if the method is overriden.
-            return Method.Attributes.Overriders.Length > 0;
-        }
-
-        public override string GetTitle() => Method.Attributes.Overriders.Length + " implements";
-
-        public override JArray GetArguments() => new JArray {
-            // Uri
-            JToken.FromObject(_script.Uri.ToString()),
-            // Range
-            JToken.FromObject(Range.start),
-            // Locations
-            JToken.FromObject(Method.Attributes.Overriders.Select(overrider => overrider.DefinedAt))
-        };
     }
 }
