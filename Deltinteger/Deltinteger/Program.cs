7535ef14d401270a7af77fb99f4da4ceb3e21939
ItsDeltin <34138844+ItsDeltin@users.noreply.github.com>
2020-03-13 13:50:07 -0400

    Update version.

--- Deltinteger/Deltinteger/Program.cs
+++ Deltinteger/Deltinteger/Program.cs
@@ -15,7 +15,7 @@ namespace Deltin.Deltinteger
 {
     public class Program
     {
-        public const string VERSION = "v1.2";
+        public const string VERSION = "v1.2.2";
 
         public static readonly string ExeFolder = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
 
