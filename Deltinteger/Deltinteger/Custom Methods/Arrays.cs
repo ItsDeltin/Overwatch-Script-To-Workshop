01f686ad0211d039b1ce184816cc9eddde5ea7bf
TrueCP5 <truecp5@gmail.com>
2020-04-03 08:30:01 +0200

    Added AverageOfArray

--- Deltinteger/Deltinteger/Custom Methods/Arrays.cs
+++ Deltinteger/Deltinteger/Custom Methods/Arrays.cs
@@ -24,4 +24,26 @@ namespace Deltin.Deltinteger.CustomMethods
             return sum.GetVariable();
         }
     }
+
+    [CustomMethod("AverageOfArray", "Gets the average of an array.", CustomMethodType.MultiAction_Value)]
+    class AverageOfArray : CustomMethodBase
+    {
+        public override CodeParameter[] Parameters() => new CodeParameter[]
+        {
+            new CodeParameter("Array", "The array to average.")
+        };
+
+        public override IWorkshopTree Get(ActionSet actionSet, IWorkshopTree[] parameterValues)
+        {
+            IndexReference array = actionSet.VarCollection.Assign("_arrayToAverage", actionSet.IsGlobal, true);
+            IndexReference sum = actionSet.VarCollection.Assign("_sumOfAverageArray", actionSet.IsGlobal, true);
+
+            actionSet.AddAction(array.SetVariable((Element)parameterValues[0]));
+            actionSet.AddAction(sum.SetVariable(0));
+            ForeachBuilder builder = new ForeachBuilder(actionSet, array.GetVariable());
+            actionSet.AddAction(sum.ModifyVariable(Operation.Add, builder.IndexValue));
+            builder.Finish();
+            return (Element)sum.GetVariable() / Element.Part<V_CountOf>(array.GetVariable());
+        }
+    }
 }
