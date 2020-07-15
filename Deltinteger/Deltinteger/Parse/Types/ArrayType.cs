6dbb0c1b599ebfec2928ec1dd723c230b56b7407
ItsDeltin <grittydeltin@gmail.com>
2020-07-15 19:46:09 -0400

    New array shorthands.

--- Deltinteger/Deltinteger/Parse/Types/ArrayType.cs
+++ Deltinteger/Deltinteger/Parse/Types/ArrayType.cs
@@ -29,47 +29,42 @@ namespace Deltin.Deltinteger.Parse
             _scope.AddNativeVariable(_first);
 
             // Filtered Array
-            Func(new FuncMethodBuilder() {
+            new GenericSortFunction() {
                 Name = "FilteredArray",
                 Documentation = "A copy of the specified array with any values that do not match the specified condition removed.",
-                DoesReturnValue = true,
                 ReturnType = this,
-                Parameters = new CodeParameter[] {
-                    new CodeParameter("conditionLambda", "The condition that is evaluated for each element of the copied array. If the condition is true, the element is kept in the copied array.", new MacroLambda(null, ArrayOfType))
-                },
-                Action = (actionSet, methodCall) => GenericSort<V_FilteredArray>(actionSet, methodCall)
-            });
+                ArrayOfType = ArrayOfType,
+                ParameterDocumentation = "The condition that is evaluated for each element of the copied array. If the condition is true, the element is kept in the copied array."
+            }.Add<V_FilteredArray>(_scope);
             // Sorted Array
-            Func(new FuncMethodBuilder() {
+            new GenericSortFunction() {
                 Name = "SortedArray",
                 Documentation = "A copy of the specified array with the values sorted according to the value rank that is evaluated for each element.",
-                DoesReturnValue = true,
                 ReturnType = this,
-                Parameters = new CodeParameter[] {
-                    new CodeParameter("conditionLambda", "The value that is evaluated for each element of the copied array. The array is sorted by this rank in ascending order.", new MacroLambda(null, ArrayOfType))
-                },
-                Action = (actionSet, methodCall) => GenericSort<V_SortedArray>(actionSet, methodCall)
-            });
+                ArrayOfType = ArrayOfType,
+                ParameterDocumentation = "The value that is evaluated for each element of the copied array. The array is sorted by this rank in ascending order."
+            }.Add<V_SortedArray>(_scope);
             // Is True For Any
-            Func(new FuncMethodBuilder() {
+            new GenericSortFunction() {
                 Name = "IsTrueForAny",
                 Documentation = "Whether the specified condition evaluates to true for any value in the specified array.",
-                DoesReturnValue = true,
-                Parameters = new CodeParameter[] {
-                    new CodeParameter("conditionLambda", "The condition that is evaluated for each element of the specified array.", new MacroLambda(null, ArrayOfType))
-                },
-                Action = (actionSet, methodCall) => GenericSort<V_IsTrueForAny>(actionSet, methodCall)
-            });
+                ArrayOfType = ArrayOfType,
+                ParameterDocumentation = "The condition that is evaluated for each element of the specified array."
+            }.Add<V_IsTrueForAny>(_scope);
             // Is True For All
-            Func(new FuncMethodBuilder() {
+            new GenericSortFunction() {
                 Name = "IsTrueForAll",
                 Documentation = "Whether the specified condition evaluates to true for every value in the specified array.",
-                DoesReturnValue = true,
-                Parameters = new CodeParameter[] {
-                    new CodeParameter("conditionLambda", "The condition that is evaluated for each element of the specified array.", new MacroLambda(null, ArrayOfType))
-                },
-                Action = (actionSet, methodCall) => GenericSort<V_IsTrueForAll>(actionSet, methodCall)
-            });
+                ArrayOfType = ArrayOfType,
+                ParameterDocumentation = "The condition that is evaluated for each element of the specified array."
+            }.Add<V_IsTrueForAll>(_scope);
+            // Mapped
+            new GenericSortFunction() {
+                Name = "Map",
+                Documentation = "Whether the specified condition evaluates to true for every value in the specified array.",
+                ArrayOfType = ArrayOfType,
+                ParameterDocumentation = "The condition that is evaluated for each element of the specified array."
+            }.Add<V_MappedArray>(_scope);
             // Contains
             Func(new FuncMethodBuilder() {
                 Name = "Contains",
@@ -160,5 +155,43 @@ namespace Deltin.Deltinteger.Parse
         public override Scope GetObjectScope() => _scope;
         public override Scope ReturningScope() => null;
         public override CompletionItem GetCompletion() => throw new NotImplementedException();
+    }
+
+    class GenericSortFunction
+    {
+        public string Name;
+        public string Documentation;
+        public string ParameterDocumentation;
+        public CodeType ReturnType;
+        public CodeType ArrayOfType;
+
+        public void Add<T>(Scope addToScope) where T: Element, new()
+        {
+            // value => ...
+            var noIndex = GetFuncMethod();
+            noIndex.Parameters = new CodeParameter[] {
+                new CodeParameter("conditionLambda", ParameterDocumentation, new MacroLambda(null, ArrayOfType))
+            };
+            noIndex.Action = (actionSet, methodCall) =>
+                Element.Part<T>(actionSet.CurrentObject, ((LambdaAction)methodCall.ParameterValues[0]).Invoke(actionSet, new V_ArrayElement()));
+
+            // (value, index) => ...
+            var withIndex = GetFuncMethod();
+            withIndex.Parameters = new CodeParameter[] {
+                new CodeParameter("conditionLambda", ParameterDocumentation, new MacroLambda(null, ArrayOfType, null))
+            };
+            withIndex.Action = (actionSet, methodCall) =>
+                Element.Part<T>(actionSet.CurrentObject, ((LambdaAction)methodCall.ParameterValues[0]).Invoke(actionSet, new V_ArrayElement(), new V_CurrentArrayIndex()));
+            
+            addToScope.AddNativeMethod(new FuncMethod(noIndex));
+            addToScope.AddNativeMethod(new FuncMethod(withIndex));
+        }
+
+        private FuncMethodBuilder GetFuncMethod() => new FuncMethodBuilder() {
+            Name = Name,
+            Documentation = Documentation,
+            ReturnType = ReturnType,
+            DoesReturnValue = true
+        };
     }
 }
