0372137f3331f2d31fb5ed5c1b6611279e010ed1
ItsDeltin <34138844+ItsDeltin@users.noreply.github.com>
2021-02-07 01:23:10 -0500

    v2.0-beta.10 -> v2.0-beta.11

--- Deltinteger/Deltinteger/publish.ps1
+++ Deltinteger/Deltinteger/publish.ps1
@@ -1,6 +1,6 @@
 $configuration = 'Release'
 $framework = 'netcoreapp3.0'
-$ostw_version = 'v2.0-beta.10'
+$ostw_version = 'v2.0-beta.11'
 
 # Cross platform, no runtime included.
 '* Publishing self-contained'
