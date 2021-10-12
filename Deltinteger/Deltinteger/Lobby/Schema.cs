655e13a2f63d638653be930882e01ce78e64e107
CactusPuppy <puppycactus@gmail.com>
2021-10-12 11:45:38 -0700

    fix: Remove misplaced attribute
    This schema is more general than Workshop itself

--- Deltinteger/Deltinteger/Lobby/Schema.cs
+++ Deltinteger/Deltinteger/Lobby/Schema.cs
@@ -24,9 +24,6 @@ namespace Deltin.Deltinteger.Lobby
         [JsonProperty("description")]
         public string Description;
 
-        [JsonProperty("modeName")]
-        public string ModeName;
-
         [JsonProperty("definitions")]
         public Dictionary<string, RootSchema> Definitions;
 
