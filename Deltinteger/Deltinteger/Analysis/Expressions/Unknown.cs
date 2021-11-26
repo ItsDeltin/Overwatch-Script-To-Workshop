df82c5b042ff3dc2273515ae63378e1c88e980e1
ItsDeltin <grittydeltin@gmail.com>
2021-11-26 18:34:05 -0500

    Unknown expression type

--- Deltinteger/Deltinteger/Analysis/Expressions/Unknown.cs
+++ Deltinteger/Deltinteger/Analysis/Expressions/Unknown.cs
@@ -1,6 +1,12 @@
 namespace DS.Analysis.Expressions
 {
+    using Types.Standard;
+
     class UnknownExpression : Expression
     {
+        public UnknownExpression()
+        {
+            Type = StandardTypes.Unknown.Director;
+        }
     }
 }
