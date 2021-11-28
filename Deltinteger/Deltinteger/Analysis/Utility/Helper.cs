94e24e92752c43d53f866fc079e3d3e504e93511
ItsDeltin <grittydeltin@gmail.com>
2021-11-28 13:09:11 -0500

    Observer select

--- Deltinteger/Deltinteger/Analysis/Utility/Helper.cs
+++ Deltinteger/Deltinteger/Analysis/Utility/Helper.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace DS.Analysis.Utility
 {
     using Types;
@@ -9,5 +11,19 @@ namespace DS.Analysis.Utility
         /// <summary>Creates a new CodeType ObserverCollection initialized with StandardTypes.Unknown.Instance.</summary>
         /// <returns>The newly initialized CodeType ObserverCollection.</returns>
         public static ObserverCollection<CodeType> CreateTypeObserver() => new ValueObserverCollection<CodeType>(StandardTypes.Unknown.Instance);
+
+
+        /// <summary>Subscribes an observer of differing type to an observable sequence.</summary>
+        /// <param name="source">The observable sequence that will be subscribed to.</param>
+        /// <param name="output">The observer which the observable will broadcast notifications to.</param>
+        /// <param name="selector">A transform function to apply to each new value.</param>
+        /// <typeparam name="TIn">The type of the source observable.</typeparam>
+        /// <typeparam name="TOut">The type of the output observer.</typeparam>
+        /// <returns>IDisposable object used to unsubscribe from the observable sequence.</returns>
+        public static IDisposable Select<TIn, TOut>(this IObservable<TIn> source, IObserver<TOut> output, Func<TIn, TOut> selector) => source.Subscribe(
+            onNext => output.OnNext(selector(onNext)),
+            onError => output.OnError(onError),
+            () => output.OnCompleted()
+        );
     }
 }
