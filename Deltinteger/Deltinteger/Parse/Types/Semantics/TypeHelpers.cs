6469f78132b954ff2a210619cdfa5e0361a840e7
ItsDeltin <grittydeltin@gmail.com>
2023-02-23 22:40:08 -0600

    IsVoid stub

--- Deltinteger/Deltinteger/Parse/Types/Semantics/TypeHelpers.cs
+++ Deltinteger/Deltinteger/Parse/Types/Semantics/TypeHelpers.cs
@@ -10,11 +10,16 @@ namespace Deltin.Deltinteger.Parse
         public static ICodeTypeInitializer[] TypesFromName(this Scope scope, string name)
         {
             var types = new List<ICodeTypeInitializer>();
-            scope.IterateParents(scope => {
+            scope.IterateParents(scope =>
+            {
                 types.AddRange(scope.Types.Where(t => t.Name == name));
                 return false;
             });
             return types.ToArray();
         }
+
+        /// <summary>There should be a special CodeType for void rather than it being null.
+        /// This is here to make it more clear when we are testing if a type is void.</summary>
+        public static bool IsVoid(CodeType type) => type == null;
     }
 }
