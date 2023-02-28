16917de49e31d1378941e680aeb68429dcf59265
ItsDeltin <grittydeltin@gmail.com>
2023-02-28 11:30:34 -0600

    Protected type linker instantiation

--- Deltinteger/Deltinteger/Parse/Types/Generics/InstanceAnonymousTypeLinker.cs
+++ Deltinteger/Deltinteger/Parse/Types/Generics/InstanceAnonymousTypeLinker.cs
@@ -12,11 +12,10 @@ namespace Deltin.Deltinteger.Parse
         public Dictionary<AnonymousType, CodeType> Links { get; } = new Dictionary<AnonymousType, CodeType>();
 
         // Creates a type linker from an array of type-args and an array of type-values.
-        // An exception will be thrown if they are not the same length.
         public InstanceAnonymousTypeLinker(AnonymousType[] typeArgs, CodeType[] typeValues)
         {
             for (int i = 0; i < typeArgs.Length; i++)
-                Links.Add(typeArgs[i], typeValues[i]);
+                Links.Add(typeArgs[i], i < typeValues.Length ? typeValues[i] : typeArgs[i]);
         }
 
         // Empty linker.
