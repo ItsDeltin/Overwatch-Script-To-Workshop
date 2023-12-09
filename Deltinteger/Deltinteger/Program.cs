f19b71657515505ee2fa2d38e58f8c6179a340a5
CactusPuppy <puppycactus@gmail.com>
2023-12-09 12:36:02 -0800

    build: bump langserver version number

--- Deltinteger/Deltinteger/Program.cs
+++ Deltinteger/Deltinteger/Program.cs
@@ -17,7 +17,7 @@ namespace Deltin.Deltinteger
 {
     public class Program
     {
-        public const string VERSION = "v2.8.5";
+        public const string VERSION = "v2.8.7";
 
         public static readonly string ExeFolder = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
 
