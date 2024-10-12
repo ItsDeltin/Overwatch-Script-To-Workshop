cc7cb597c458875ad0025b06542ec42763d10985
CactusPuppy <puppycactus@gmail.com>
2024-10-12 10:36:50 -0700

    style: Remove trailing whitespace

--- Deltinteger/Deltinteger/Elements/Rule.cs
+++ Deltinteger/Deltinteger/Elements/Rule.cs
@@ -63,7 +63,7 @@ namespace Deltin.Deltinteger.Elements
                 .AppendKeywordLine("event")
                 .AppendLine("{")
                 .Indent();
-            
+
             ElementRoot.Instance.GetEnumValue("Event", RuleEvent.ToString()).ToWorkshop(builder, ToWorkshopContext.Other);
             builder.Append(";").AppendLine();
 
@@ -125,7 +125,7 @@ namespace Deltin.Deltinteger.Elements
 
                 foreach (var action in Actions)
                     action.ToWorkshop(builder, ToWorkshopContext.Action);
-                
+
                 builder.SetCurrentIndent(resetIndentInCaseOfUnbalance);
                 builder.Outdent().AppendLine("}");
             }
